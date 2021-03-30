07df7353ac452f9ed4dda281bc844dff449b9af2
ABHI <am@MacBook-Pro.local>
2021-03-30 14:51:06 -0700

    imports Link from react-router-dom and link the pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import TodosContainer from './containers/TodosContainer';
@@ -6,6 +7,7 @@ import TodosContainer from './containers/TodosContainer';
 function App() {
   return (
     <div className="container">
+      <Header/>
       <Switch>
         <Route exact path='/' component={ Home } />
         <Route path='/todos' component={ TodosContainer }/>
