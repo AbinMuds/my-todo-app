2014f78e04fe733cf765facbee18f20d5c7b4bf7
ABHI <am@MacBook-Pro.local>
2021-03-30 14:39:04 -0700

    adds homepage and todoscontainer page for starter

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class TodosContainer extends Component {
+    render() {
+        return (
+        <>
+            <h2>I am TodosCOntianer</h2>   
+        </>
+        )
+    }
+}
+
