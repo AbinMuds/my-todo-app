a14eb304d943f47c79d656be4aa27b66f1ed45f5
ABHI <am@MacBook-Pro.local>
2021-03-31 07:48:34 -0700

    the switch are added to routes and routes is called from the routes.js to simplify the code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+import routes from './config/routes';
 
 function App() {
   return (
     <div className="container">
       <Header/>
-      <Switch>
-        <Route exact path='/' component={ Home } />
-        <Route path='/todos' component={ TodosContainer }/>
-      </Switch>
+      { routes }
     </div>
   );
 }
