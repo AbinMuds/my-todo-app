f80a0d70bc1f585ec0794bbb3755942e69aaf102
ABHI <am@MacBook-Pro.local>
2021-03-31 07:57:42 -0700

    adds parent for todos to display todos from api, fetches the api and pass todo to childs and also adds createTodo functionality

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import TodoModel from '../models/Todo';
+import Todos from '../components/Todos';
+import CreateTodoForm from '../components/CreateTodoForm';
 
 export default class TodosContainer extends Component {
+    state = {
+        todos: [],
+    }
+
+    createTodo = (todo) => {
+        let newTodo = {
+            body:todo,
+            completed:false,
+        }
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = () => {
+        TodoModel.all().then((res) => {
+            this.setState({
+                todos:res.data.todos,
+            })
+        });
+    }
     render() {
+        
         return (
-        <>
-            <h2>I am TodosCOntianer</h2>   
-        </>
+            <div className='todosContainer'>
+                <CreateTodoForm 
+                    createTodo={this.createTodo} />
+                <Todos
+                    todos={this.state.todos} />
+             </div>
         )
     }
 }
